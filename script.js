63843491ae6efd4856329209e55f537e87e0dc0d
Anusri <avulaanusri18@gmail.com>
2026-07-01 18:21:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,7 @@ let pages = document.querySelectorAll(".page");
 pages.forEach(p => p.classList.remove("active"));
 document.getElementById(page).classList.add("active");
 }
+setTimeout(() => {
+document.getElementById("splash").style.display = "none";
+document.querySelector(".topbar").style.display = "flex";
+}, 2500);
