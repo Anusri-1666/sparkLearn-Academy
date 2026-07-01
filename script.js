54edfd78965c0b76bf20ec5639054829a6009fa4
Anusri <avulaanusri18@gmail.com>
2026-07-01 18:12:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,9 @@
-// splash
 setTimeout(() => {
 document.getElementById("splash").style.display = "none";
-}, 2000);
+}, 2500);
 
-// login
-setTimeout(() => {
-document.getElementById("login").style.display = "none";
-}, 4000);
-
-// dashboard navigation
-function showPage(pageId) {
-
-// hide all pages
+function showPage(page){
 let pages = document.querySelectorAll(".page");
 pages.forEach(p => p.classList.remove("active"));
-
-// show selected page
-document.getElementById(pageId).classList.add("active");
-}
-function login() {
-
-let user = document.getElementById("username").value;
-let pass = document.getElementById("password").value;
-
-if(user === "" || pass === "") {
-alert("Please enter details");
-return;
-}
-
-// hide login screen
-document.getElementById("login").style.display = "none";
-
-// optional message
-alert("Login Successful ✔ Welcome " + user);
-
+document.getElementById(page).classList.add("active");
 }
