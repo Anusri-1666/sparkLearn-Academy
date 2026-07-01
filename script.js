68ead5b143f94a2f265cb7c924ad99e2939ebf5f
Anusri <avulaanusri18@gmail.com>
2026-07-01 17:58:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,20 @@ pages.forEach(p => p.classList.remove("active"));
 // show selected page
 document.getElementById(pageId).classList.add("active");
 }
+function login() {
+
+let user = document.getElementById("username").value;
+let pass = document.getElementById("password").value;
+
+if(user === "" || pass === "") {
+alert("Please enter details");
+return;
+}
+
+// hide login screen
+document.getElementById("login").style.display = "none";
+
+// optional message
+alert("Login Successful ✔ Welcome " + user);
+
+}
